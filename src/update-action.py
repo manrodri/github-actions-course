0eb8029c031ff3cf257d9caf26009a4cadcaac61
manrodri <32561150+manrodri@users.noreply.github.com>
2023-04-21 13:49:15 +0100

    update script

--- src/update-action.py
+++ src/update-action.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     # load json into python object
-    with open('./actions.json', "r") as file:
+    with open('./src/actions.json', "r") as file:
         
         data = json.load(file)
         actions = data['actions']
