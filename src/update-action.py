0272a3bb73f211aa5a876d5e987e0f9707c83b8f
manrodri <32561150+manrodri@users.noreply.github.com>
2023-04-21 13:26:09 +0100

    use dependabot pr data on script

--- src/update-action.py
+++ src/update-action.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def main():
     # load json into python object
@@ -8,20 +9,31 @@ def main():
         actions = data['actions']
     
     # we pass valid versions that's going to come from dependanbot action.
-    dependecy_to_update = {
-        "name": "checkout",
-        "versions": ["v3"],
-    }
+    action_name = os.environ.get('ACTION_NAMES')
+    new_version = os.environ.get('NEW_VERSION')
+    previous_version = os.environ.get('PREVIOUS_VERSION')
+
+    if not action_name or not new_version or not previous_version:
+        raise OSError("PR action details not available. ")
     
-    # update dependecy
-    for action in actions:
-        if dependecy_to_update["name"] == action["name"]:
-            action['versions'] = dependecy_to_update['versions']
-            print(f"Dependency {action['name']} updated")
+
+    [print(os.environ.get(var)) for var in ['ACTION_NAMES', 'NEW_VERSION', 'PREVIOUS_VERSION']]
     
-    with open('./actions.json', 'w') as file:
-        data["actions"] = actions
-        json.dump(data, file)
+    # CHANGE_DETECTED = False
+    # # update actions
+    # for action in actions:
+    #     if action_name == action["name"]:
+    #         action['versions'].remove(previous_version)
+    #         if new_version not in action['versions']:
+    #             action['versions'].append(new_version)
+
+    #         print(f"Dependency {action['name']} updated")
+    #         print(f"{action['name']} new versions are:  {action['versions']}")
+    #         CHANGE_DETECTED = True
+    # if CHANGE_DETECTED:
+    #     with open('./actions.json', 'w') as file:
+    #         data["actions"] = actions
+    #         json.dump(data, file)
 
 
 if __name__ == '__main__':
